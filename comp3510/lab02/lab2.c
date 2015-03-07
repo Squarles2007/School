b304bfaafac6fa5494f155929a6893d9aa4a72d3
kullen <kaw0026@auburn.edu>
2015-03-07 17:04:08 -0600

    kw

--- comp3510/lab02/lab2.c
+++ comp3510/lab02/lab2.c
@@ -38,10 +38,11 @@
 /*****************************************************************************\
 *                                  Global data                                *
 \*****************************************************************************/
-Event PriorityEventArray[100 * 32] = {NULL};
+Event PriorityEventArray[100 * 32];
 int pIndex = -1;
-double RT[MAX_NUMBER_DEVICES];
-double TT[MAX_NUMBER_DEVICES];
+double RT[MAX_NUMBER_DEVICES] = {0,0,0,0,0,0,0,0};
+double TT[MAX_NUMBER_DEVICES] = {0,0,0,0,0,0,0,0};
+int EventCount[MAX_NUMBER_DEVICES] = {0,0,0,0,0};
 
 
 /*****************************************************************************\
@@ -51,7 +52,8 @@ double TT[MAX_NUMBER_DEVICES];
 void Control(void);
 void InterruptRoutineHandlerDevice(void);
 void BookKeeping();
-void printArray();
+void insert(Event event);
+
 
 
 /*****************************************************************************\
@@ -80,15 +82,17 @@ int main (int argc, char **argv) {
  * Function: Monitor Devices and process events (written by students)    *
  \***********************************************************************/
 void Control(void){
-	
+  Event event;
   while (1){
-	if(pIndex < -1){
-		
-		printf("SERVER----------------------------------");
-		Server(&PriorityEventArray[pIndex]);
+	if(pIndex > -1){
+		event = PriorityEventArray[pIndex];
 		pIndex--;
-
-
+		Server(&event);
+		
+		TT[event.DeviceID] += Now() - event.When;
+		
+		//incr number of events process per device
+		EventCount[event.DeviceID] = EventCount[event.DeviceID] + 1;
 	}
 
   } //end while(1)
@@ -105,18 +109,21 @@ void Control(void){
 void InterruptRoutineHandlerDevice(void){
  	printf("An event occured at %f  Flags = %d \n", Now(), Flags);
 	// Put Here the most urgent steps that cannot wait
-	Event event;
 	Status CurrentStatus = Flags;
+	Flags = 0;
+	Event event;
 	int position = 0;	
 
-	Flags = 0;
-	
 	while(CurrentStatus){
-		event = BufferLastEvent[position];		
+				
 		if(CurrentStatus & 1){
+			event = BufferLastEvent[position];
 			RT[event.DeviceID] += Now() - event.When;
 			DisplayEvent('x', &event);
 			insert(event);
+			//pIndex++;
+			//PriorityEventArray[pIndex] = event;
+		
 		} // end if
 
 		position++;
@@ -137,36 +144,46 @@ void BookKeeping(void){
   // 1) the percentage of missed events, 2) the average response time, and 
   // 3) the average turnaround time.
   // Print the overall averages of the three metrics 1-3 above
+  int i = 0;
+  while(EventCount[i] > 0){
+	printf("\nDevice %d processed %d events\n",i,EventCount[i]);
+	printf("	AVG Response Time %10.3f\n", RT[i]/100);
+	printf("	AVG Turnaroun Time %10.3f\n", TT[i]/100);
+		
+	i++;
+  }
+
+  printf("DONE\n");
 }
 
 /***********************************************************************\
 INSERT performs insertion sort on PriotyEventArray to place events in
 priority order.
+Lowest Priority number has highest priority
 
 \***********************************************************************/
 void insert(Event event){
+	
 	pIndex++;	
-	printf("INSERT P-INDEX = %d\n",pIndex);
+	//printf("INSERT P-INDEX = %d\n",pIndex);
 	if(pIndex == 0){
 		PriorityEventArray[0] = event;
 	}
 	else {
-		PriorityEventArray[pIndex] = event;
+		
 		int key;
 		int j;
 		Event keyEvent;
-		int i = 1;
-		for( i ; i < pIndex; i++){
-			keyEvent = PriorityEventArray[i];
-			key = PriorityEventArray[i].priority;
-			j = i-1;
-			while(j >= 0 && key > PriorityEventArray[j].priority)
-			{
-				PriorityEventArray[j-1] = PriorityEventArray[j];
-				j--;
-			}
-			PriorityEventArray[j+1] = keyEvent;
+		keyEvent = event;
+
+		j = pIndex-1;
+		key = PriorityEventArray[pIndex].priority;
+	
+		while(key > PriorityEventArray[j].priority && j >= 0){
+			PriorityEventArray[j-1] = PriorityEventArray[j];
+			j--;
 		}
+		PriorityEventArray[j+1] = keyEvent;
 
 	} //end else
 		

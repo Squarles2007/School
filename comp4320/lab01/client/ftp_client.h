e5624dab160c71e37815fdadf42c7348fb8a20fe
kullen <kullen@auburn.edu>
2015-07-02 16:24:45 -0500

    update

---
+++ comp4320/lab01/client/ftp_client.h
@@ -0,0 +1,36 @@
+//ftp_client.h
+
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring> 
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/poll.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <stdint.h>
+
+#define BUFSIZE 128
+#define DATASIZE 126
+#define HEADERSIZE 2
+#define PORT 10026
+#define MAXLINE 1024
+
+typedef struct {
+    uint8_t Sequence;
+    unsigned char Checksum;
+    char Data[DATASIZE];
+} Packet;
+
+class Client{
+
+
+};

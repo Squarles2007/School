708fe18af9c0a1bf470a79a7a6dd4ef5a2ff29a7
kullen <kullen@auburn.edu>
2015-07-02 11:49:42 -0500

    makefile changes
    added comments and updated make file to print run instructions

--- comp4320/lab01/client/ftp_client.cpp
+++ comp4320/lab01/client/ftp_client.cpp
@@ -1,39 +1,15 @@
 //compile: make
 //run : ./ftp_client d <num> l <num> s <ipaddress>
 
-
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <cstring> 
-#include <vector>
-#include <sstream>
-#include <fstream>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/poll.h>
-
-#include <arpa/inet.h>
-#include <netinet/in.h>
-
-#include <stdint.h>
-
-#define BUFSIZE 128
-#define DATASIZE 126
-#define HEADERSIZE 2
-#define PORT 10026
-#define MAXLINE 1024
+#include "ftp_client.h"
 
 using namespace std;
 
-typedef struct {
-    uint8_t Sequence;
-    unsigned char Checksum;
-    char Data[DATASIZE];
-} Packet;	
+	
 
+void commandArg();
 void help();
+void put();
 char generateChecksum(char*, int);
 bool gremlin(float, float, Packet*);
 char* loadFileToBuffer();
@@ -56,12 +32,11 @@ int main(int argc, char *argv[])
 
     //Command line arguments
     string sServerAddress;
-   
-    float fDamaged = 0;
-    float fLost = 0;
+    float fDamaged = 0;    //Default values for damage and loss
+    float fLost = 0;	      //If user only provides ip address values are 0
 
-	bool bSent = false;
-	bool bGremlin = false;
+    bool bSent = false;
+    bool bGremlin = false;
 
     //Sending variables
     Packet *pPacket;
@@ -75,81 +50,88 @@ int main(int argc, char *argv[])
 	return 0;
     }
     
-    /* ================================
+    /* ========================================================================
     *   read in command line args
-    *  ================================*/
+    *  =======================================================================*/
     char *switchState;
-    for(int i=1;i < argc; i+= 2)
-    {
-	 switchState = argv[i];
-        switch (switchState[0])
-        {
+
+    for(int i=1;i < argc; i+= 2) {
+	    switchState = argv[i];
+        switch (switchState[0]) {
             case 'd': {
                 fDamaged = atof(argv[i+1]);
-		  cout << "Damaged "<< fDamaged <<endl;
+                cout << "\nPacket Damaged "<< fDamaged <<endl;
             };
             break;
 
             case 'l': {
                 fLost = atof(argv[i+1]);
-		  cout << "Lost " << fLost << endl;
+		  		cout << "\nPacket Loss " << fLost << endl;
             };
             break;
 
             case 's': {
                 sServerAddress = argv[i+1];
- 		  cout << "IP " << sServerAddress << endl;
+ 		  		cout << "\nIP address " << sServerAddress << endl;
             };
             break;
 
             case 'h': {   //help  
-            }
-	     default: {
-		help();
-              return 0;
-	    }
+            	help();
+            	return 0;
+            };
+            break;
+	     	default: {
+				commandArg();
+              	return 0;
+	    	}
 
         }  //end switch
-    } //end loop
+    } //end loop ==============================================================
 
-    //Loop forever
+
+    /* ========================================================================
+    *   Loop forever
+    *  ========================================================================*/
     string ln;
-    for(;;)
-    {
-		//Open socket
+    for(;;) {
+        //Open socket
 		if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-			cerr << "Unable to open socket. Exiting..." << endl;
-			return 0;
-		}
+            cout << "Unable to open socket. Exiting..." << endl;
+	     	return 0;
+	 	}
 
 		bzero(&serverAddress, sizeof(serverAddress));
 		serverAddress.sin_family = AF_INET;
 		serverAddress.sin_port = htons(PORT);
 
-		if (inet_aton(sServerAddress.c_str(), &serverAddress.sin_addr)==0)
-		{
-			cerr << "There was a problem with the server IP address. Exiting..." << endl;
+		if (inet_aton(sServerAddress.c_str(), &serverAddress.sin_addr)==0) {
+			cout << "There was a problem with the server IP address. Exiting..." << endl;
 			return 0;
 		}
 
-		cout << "Type command or quit to quit: ";
+		cout << "Enter PUT command to transfer file or quit to exit: ";
 		cin.clear();
 		cin >> ln;
-		if(ln.compare("quit") == 0)
-			break;
-		if(ln.compare("PUT") == 0 || ln.compare("put") == 0)
-		{
+		string command = ln;
+		cout << command << endl;
+		if(ln.compare("quit") == 0) {	break; }
+		
+		if(ln.compare("PUT") == 0 || ln.compare("put") == 0) {
+			cin.clear();
+			cout << "Enter File Name: ";
 			cin >> ln;
+			string filename = ln;
+			cout << filename << endl;
 			//Now ln is our filename to send
 			ifstream putfile;
 			putfile.open(ln.c_str());
 			unsigned char csum = 0x00;
 			unsigned char lost = 0x00;
 			char buff[DATASIZE];
-			if(putfile.is_open())
-			{
+			if(putfile.is_open())  {
 			
-				//Now we send a 1 packet overhead for the filename
+				//Now send a 1 packet overhead for the filename
 			
 				bSent = false;
 				pPacket = constructPacket((char*)"PUT TestFile", strlen("PUT TestFile"));
@@ -160,25 +142,21 @@ int main(int argc, char *argv[])
 					cout << "Sending put..." << endl;
 				}	
 			
-				while(!putfile.eof())
-				{
+				while(!putfile.eof()) {
 
-					/************************************************/
-					// This part of the code reads in from the      //
-					// open file and fills up the data part of      //
-					// the packet. It also calculates the checksum. //
-					/************************************************/
-					for(int i = 0; i < DATASIZE; i++)
-					{
-						if(!putfile.eof())
-						{
+					/*============================================
+					* This part of the code reads in from the      
+					* open file and fills up the data part of      
+					* the packet. It also calculates the checksum. 
+					===============================================*/
+					for(int i = 0; i < DATASIZE; i++) {
+						if(!putfile.eof()) {
 							if( i < 48 ) {
 								cout << buff[i];
 							}
 							buff[i] = putfile.get();
 						}
-						else
-						{
+						else {
 							if( i != 0 ) {
 								buff[i-1]= '\0';
 							}
@@ -188,14 +166,9 @@ int main(int argc, char *argv[])
 					cout << endl;
 				
 
-					/*************************************************/
-					// This part of the code looks at the input      //
-					// parameters and determines if a packet should  //
-					// be simulated lost or damaged. This is also    //
-	  				// known as the GREMLIN function   		 //
-					/*************************************************/
-
-					//Send
+					/*================================================
+					* GREMLIN function:	 
+					==================================================*/
 					bSent = false;
 					pPacket = constructPacket(buff, strlen(buff));
 					while( !bSent ) {
@@ -219,117 +192,74 @@ int main(int argc, char *argv[])
 
 		}
 		close(fd);
-    }
+    }  //END loop forever =====================================================
 
     return 0;
 }
 
-bool gremlin(float fDamaged, float fLost, Packet* ppacket)
-{
-	if(fLost > (1.0 * rand()) / (1.0 * RAND_MAX))
-	{
-		return true;
-	}
 
-	if(fDamaged > (1.0 * rand()) / (1.0 * RAND_MAX))
-	{
-		int numDamaged = rand() % 10;
-		int byteNum = rand() % BUFSIZE;
-		if(numDamaged == 9)
-		{
-			if(numDamaged > 1)
-			{
-				ppacket->Data[numDamaged-HEADERSIZE]+= 8;	
-			}
-			else if(numDamaged == 1)
-			{
-				ppacket->Checksum+= 8;
-			}
-			else
-			{
-				ppacket->Sequence+= 8;
-			}
+/*=============================================================================
+* GREMLIN function:	
+* Randomly determines, depending on the damage probability
+* whether to corrupt some bits or pass the packets as it is to the server
+* receiving function.
+* It will also decide whether some packets will be dropped based on the 
+* loss probability. 
+* (more information in README under GREMLIN FUNCTION)
+*=================================================================================*/
+bool gremlin(float fDamaged, float fLost, Packet* ppacket) {
+    
+    if(fLost > (1.0 * rand()) / (1.0 * RAND_MAX)) {
+        return true;
+    }
+
+    if(fDamaged > (1.0 * rand()) / (1.0 * RAND_MAX)) {
+        int numDamaged = rand() % 10;
+	 int byteNum = rand() % BUFSIZE;
+
+	 if(numDamaged == 9) {
+
+	     if(numDamaged > 1) { ppacket->Data[numDamaged-HEADERSIZE]+= 8; }
+	     else if(numDamaged == 1) {  ppacket->Checksum+= 8; }
+	     else {  ppacket->Sequence+= 8; }
 			
-			numDamaged = rand() % 10;
-			if(numDamaged > 1)
-			{
-				ppacket->Data[numDamaged-HEADERSIZE]+= 4;	
-			}
-			else if(numDamaged == 1)
-			{
-				ppacket->Checksum+= 4;
-			}
-			else
-			{
-				ppacket->Sequence+= 4;
-			}
+            numDamaged = rand() % 10;
+	     
+	     if(numDamaged > 1) { ppacket->Data[numDamaged-HEADERSIZE]+= 4; }
+	     else if(numDamaged == 1) {  ppacket->Checksum+= 4;  }
+	     else { ppacket->Sequence+= 4; }
 
-			numDamaged = rand() % 10;
-			if(numDamaged > 1)
-			{
-				ppacket->Data[numDamaged-HEADERSIZE]+= 2;	
-			}
-			else if(numDamaged == 1)
-			{
-				ppacket->Checksum+= 2;
-			}
-			else
-			{
-				ppacket->Sequence+= 2;
-			}
-		}
-		else if(numDamaged > 6)
-		{
-			if(numDamaged > 1)
-			{
-				ppacket->Data[numDamaged-HEADERSIZE]+= 8;	
-			}
-			else if(numDamaged == 1)
-			{
-				ppacket->Checksum+= 8;
-			}
-			else
-			{
-				ppacket->Sequence+= 8;
-			}
+            numDamaged = rand() % 10;
 
-			numDamaged = rand() % 10;
-			if(numDamaged > 1)
-			{
-				ppacket->Data[numDamaged-HEADERSIZE]+= 4;	
-			}
-			else if(numDamaged == 1)
-			{
-				ppacket->Checksum+= 4;
-			}
-			else
-			{
-				ppacket->Sequence+= 4;
-			}
-		}
-		else
-		{
-			if(numDamaged > 1)
-			{
-				ppacket->Data[numDamaged-HEADERSIZE]+=8;	
-			}
-			else if(numDamaged == 1)
-			{
-				ppacket->Checksum+=4;
-			}
-			else
-			{
-				ppacket->Sequence+= 2;
-			}
-		}
-
+	     if(numDamaged > 1) { ppacket->Data[numDamaged-HEADERSIZE]+= 2; }
+	     else if(numDamaged == 1)  { ppacket->Checksum+= 2;  }
+	     else { ppacket->Sequence+= 2; }
 	}
-	return false;
-}
+	else if(numDamaged > 6) {
 
-void help() {
-    cout << "Use the following syntax: \nproject1 l <lost packets> d <damaged packets> s <ip address>\n" << endl;
-}
+	    if(numDamaged > 1) { ppacket->Data[numDamaged-HEADERSIZE]+= 8; }
+           else if(numDamaged == 1) { ppacket->Checksum+= 8; }
+	    else{ ppacket->Sequence+= 8;	}
+
+	    numDamaged = rand() % 10;
+
+	    if(numDamaged > 1) { ppacket->Data[numDamaged-HEADERSIZE]+= 4; }
+	    else if(numDamaged == 1){ ppacket->Checksum+= 4; }
+	    else {	ppacket->Sequence+= 4; }
+	}
+	else {
+
+	    if(numDamaged > 1) {  ppacket->Data[numDamaged-HEADERSIZE]+=8; }
+	    else if(numDamaged == 1) { ppacket->Checksum+=4; }
+	    else { ppacket->Sequence+= 2; }
+	}
+
+    }
+    return false;
+} //END GREMLIN ===============================================================
+
+
+
 
 unsigned char generateChecksum( Packet* pPacket ) {
     unsigned char retVal = 0x00;
@@ -412,5 +342,28 @@ bool sendPacket(const Packet* pPacket, bool bLost) {
 	}
   
 	return bReturn;
+}
+
+
+/* ============================================================================
+*  HELP!!!!
+*  little Extra instructions
+*  ============================================================================*/
+void help() {
+	commandArg();
+}
+
+/* ============================================================================
+*  COMMAND ARGS !!!!
+*  if user trys to run program with no args or runs with ./fileName h
+*  displays proper syntax
+*  ============================================================================*/
+void commandArg(){
+	cout << "***************************************" << endl;
+    cout << "\nWHEN RUNNING: \nUse one of the following syntax\n\n./fileName l <lost packets> d <damaged packets> s <ip address>" << endl;
+    cout << "OR ./fileName d <damaged packets> s <ip address>" << endl;
+    cout << "OR ./fileName l <lost packets> s <ip address>"  <<endl;
+    cout << "OR ./fileName s <ip address>\n" <<endl;
+    cout << "***************************************" << endl;
 }
 
